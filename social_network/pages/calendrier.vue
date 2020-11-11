e4a11e47d4d0274729dc6c4611cc3fc951b2a3f4
dasco69 <claude.proaudiovisuel@gmail.com>
2020-11-11 17:56:35 +0100

    add back end and front end nuxt

---
+++ social_network/pages/calendrier.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <calendar />
+    </div>
+</template>
+
+<script>
+import Calendar from '../components/calendar/Calendar.vue'
+    export default {
+        components: {
+            Calendar,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ social_network/pages/social_network.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Réseaux sociaux</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'social_network'
+    }
+</script>
+
+<style>
+
+</style>
